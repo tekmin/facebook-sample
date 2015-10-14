6b6706e2cd4994dc586d38db35a812a84df03d1a
tekmin <tekmin.ewe@seekasia.com>
2015-10-14 19:45:04 +0800

    Initial commit

---
+++ FacebookSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookSample
+//
+//  Created by Ewe Tek Min on 14/10/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacebookSample
+//
+//  Created by Ewe Tek Min on 14/10/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
